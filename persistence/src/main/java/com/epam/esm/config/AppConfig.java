e5b82e9244c1398c98c2aa663fc75e1844567205
alekseysayarkin <alekseysayarkin@gmail.com>
2020-10-22 16:03:47 +0300

    Added spring configuration

---
+++ persistence/src/main/java/com/epam/esm/config/AppConfig.java
@@ -0,0 +1,36 @@
+package com.epam.esm.config;
+
+import com.epam.esm.dao.impl.SQLTagImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+public class AppConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/GiftShop");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public SQLTagImpl tagDAO() {
+        return new SQLTagImpl();
+    }
+}

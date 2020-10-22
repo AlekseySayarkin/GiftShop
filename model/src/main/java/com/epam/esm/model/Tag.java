7a8de9315ee5268c40414e080de391a05df58bf6
alekseysayarkin <alekseysayarkin@gmail.com>
2020-10-22 23:27:21 +0300

    Added constructor with parameters

--- model/src/main/java/com/epam/esm/model/Tag.java
+++ model/src/main/java/com/epam/esm/model/Tag.java
@@ -8,6 +8,14 @@ public class Tag {
     private int id;
     private String name;
 
+    public Tag() {
+    }
+
+    public Tag(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }

c112f3cdd0a4bcde76d876ca10e49a64325c802a
alekseysayarkin <alekseysayarkin@gmail.com>
2020-10-22 20:40:41 +0300

    Changed modules dependencies, moved spring configuration file to Web module

---
+++ web/src/main/java/com.epam.esm/config/AppConfig.java
@@ -0,0 +1,45 @@
+package com.epam.esm.config;
+
+import com.epam.esm.dao.impl.SQLTagDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+public class AppConfig {
+
+    private final Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public SQLTagDaoImpl tagDAO(JdbcTemplate jdbcTemplate) {
+        return new SQLTagDaoImpl(jdbcTemplate);
+    }
+}

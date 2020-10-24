80955e3854aa53ee6c7b7c88ca33e5c5ef86ba72
alekseysayarkin <alekseysayarkin@gmail.com>
2020-10-24 13:56:04 +0300

    Changed test

--- persistence/src/test/java/com/epam/esm/dao/SQLTagDaoImpTest.java
+++ persistence/src/test/java/com/epam/esm/dao/SQLTagDaoImpTest.java
@@ -25,43 +25,42 @@ public class SQLTagDaoImpTest {
                 .addScript("db/schema.sql")
                 .build();
 
-
         tagDao = new SQLTagDaoImpl(new JdbcTemplate(dataSource), new TagRowMapper());
     }
 
     @Test
     public void whenAddTag_thenCorrectlyDeleteId() {
-        Tag tagToDelete = new Tag(1, "Tag to return");
-        tagDao.addTag(tagToDelete);
+        Tag tagToDelete = new Tag("Tag to delete");
+        tagToDelete.setId(tagDao.addTag(tagToDelete));
         Assert.assertTrue(tagDao.deleteTag(tagToDelete));
     }
 
     @Test
     public void whenAddTag_thenReturnNonZeroId() {
-        Tag tagToAdd = new Tag(1, "Tag to add");
+        Tag tagToAdd = new Tag("Tag to add");
         Assert.assertNotEquals(0, tagDao.addTag(tagToAdd));
-
     }
 
     @Test
     public void whenAddTag_thenCorrectlyReturnIt() {
-        Tag tagToReturn = new Tag(1, "Tag to return");
+        Tag tagToReturn = new Tag("Tag to return");
         tagDao.addTag(tagToReturn);
-        Assert.assertEquals(tagToReturn, tagDao.getTagById(tagToReturn.getId()));
-        Assert.assertEquals(tagToReturn, tagDao.getTagByName(tagToReturn.getName()));
+        Assert.assertEquals(tagToReturn.getName(), tagDao.getTagByName(tagToReturn.getName()).getName());
     }
 
     @Test
     public void whenAddTags_thenCorrectlyReturnsIt() {
         List<Tag> tagsToReturn = new ArrayList<>();
-        tagsToReturn.add(new Tag(1, "Tag one"));
-        tagsToReturn.add(new Tag(2, "Tag two"));
-        tagsToReturn.add(new Tag(3, "Tag three"));
+        tagsToReturn.add(new Tag("Tag one"));
+        tagsToReturn.add(new Tag("Tag two"));
+        tagsToReturn.add(new Tag("Tag three"));
 
         for (Tag tag: tagsToReturn) {
             tagDao.addTag(tag);
         }
 
-        Assert.assertEquals(tagsToReturn, tagDao.getAllTags());
+        for (Tag tag: tagsToReturn) {
+            Assert.assertEquals(tag.getName(), tagDao.getTagByName(tag.getName()).getName());
+        }
     }
 }

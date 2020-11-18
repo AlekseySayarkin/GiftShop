20698a00f4c7fac0fa74565ba5c5fc84a99d0e4e
alekseysayarkin <alekseysayarkin@gmail.com>
2020-11-18 07:27:42 +0300

    added proper serialization of ErrorCodeEnum

--- persistence/src/main/java/com/epam/esm/dao/exception/ErrorCodeEnum.java
+++ persistence/src/main/java/com/epam/esm/dao/exception/ErrorCodeEnum.java
@@ -1,18 +1,20 @@
 package com.epam.esm.dao.exception;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ErrorCodeEnum {
 
-    FAILED_TO_RETRIEVE_CERTIFICATE(502),
-    FAILED_TO_DELETE_CERTIFICATE(503),
-    FAILED_TO_UPDATE_CERTIFICATE(504),
-    FAILED_TO_ADD_CERTIFICATE(505),
-    FAILED_TO_RETRIEVE_TAG(506),
-    FAILED_TO_DELETE_TAG(507),
-    FAILED_TO_ADD_TAG(508),
+    FAILED_TO_RETRIEVE_CERTIFICATE(50201),
+    FAILED_TO_DELETE_CERTIFICATE(50301),
+    FAILED_TO_UPDATE_CERTIFICATE(50401),
+    FAILED_TO_ADD_CERTIFICATE(50501),
+    FAILED_TO_RETRIEVE_TAG(50602),
+    FAILED_TO_DELETE_TAG(50702),
+    FAILED_TO_ADD_TAG(50802),
     INVALID_INPUT(509),
     INVALID_SORT_INPUT(510),
-    CERTIFICATE_VALIDATION_ERROR(511),
-    TAG_VALIDATION_ERROR(512);
+    CERTIFICATE_VALIDATION_ERROR(51100),
+    TAG_VALIDATION_ERROR(51201);
 
     private final int code;
 
@@ -20,6 +22,7 @@ public enum ErrorCodeEnum {
         this.code = code;
     }
 
+    @JsonValue
     public int getCode() {
         return code;
     }
